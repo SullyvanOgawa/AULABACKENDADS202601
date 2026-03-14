3f4fe0f5da3a462d0b8faacd6b26308bc37e58b8
Sullyvan Ogawa <sullyvanogawa@gmail.com>
2026-03-14 16:26:16 -0300

    test

--- Controller/clienteController.js
+++ Controller/clienteController.js
@@ -6,7 +6,7 @@ import Cliente from "../Model/cliente.js";
 export default class ClienteController {
 
     // Requisição do tipo POST
-    async gravar(requisicao, resposta) {
+    gravar(requisicao, resposta) {
 
         //Para que um requisição seja processada, o seu método (method) deve ser do tipo POST.
                                                         //mimetype - rótulos ou etiquetas para identificar o conteúdo de uma requisição HTTP.
@@ -24,8 +24,27 @@ export default class ClienteController {
                     // pseudo validação dos dados
                     if(cpf && nome && endereco && bairro && cidade && telefone && email){
                         // Por ser uma entidade (objeto). Nesse momento pegamos apenas o id da cidade especificada ao cliente.Em suma, para criar um cliente eu preciso do objeto cidade.
+                        // instanciando os objetos cidade e cliente.
                         const cidadeObj = new Cidade(cidade.id);
                         const cliente = new Cliente(0, cpf, nome, endereco, bairro, cidadeObj, telefone, email);
+
+                        // Não bloquear a execução enquanto o banco de dados não responde, o que deve ser feito é um agendamento de funções anônimas que erão processadas quando houver uma resposta. 
+                        Cliente.gravar().
+                        then(() => {
+                            resposta.status(201).json({
+                                "status": true,
+                                "mensagem": "Cliente cadastrado com sucesso.",
+                                "id": cliente.id
+                            });
+                            
+                        })
+                        .catch((erro) => {
+                            resposta.status(500).json({
+                                "status": false,
+                                "mensagem": "Erro ao gravar o cliente: " + erro.message
+                            })
+                        });
+                        
                     }
                     else{
                         resposta.status(400).json({
@@ -48,13 +67,126 @@ export default class ClienteController {
     }
 
     // Requisição do tipo PUT ou PATCH
-    async editar(requisicao, resposta) {}
+     editar(requisicao, resposta) {
+        // deveria ser implementado para substituir completamente um recurso no servidor. 
+        // deveria ser implementado para alterar parte de um recurso existente no servidor.
+        // fakestorestapi.com/docs
+        if(requisicao.method === "PUT" || requisicao.method === "PATCH" && requisicao.is("aplication/json")){
+            // pelo padrão rest o id do cliente deve ser buscado na URL, e nâo no corpo da requisição.
+            // params - parâmetros da URL.
+            const id = requisicao.params.id; // o id está na URL da requisição.
+            // o resto das informações do cliente vem no corpo da requisição.
+            const cpf = requisicao.body.cpf;
+            const nome = requisicao.body.nome;
+            const endereco = requisicao.body.endereco;
+            const bairro = requisicao.body.bairro;
+            const cidade = requisicao.body.cidade;
+            const telefone = requisicao.body.telefone;
+            const email = requisicao.body.email;
+
+            if (id > 0 && cpf && nome && endereco && bairro && cidade && telefone && email){
+                // instanciando os objetos cidade e cliente.
+                const cidadeObj = new Cidade(cidade.id);
+                const cliente = new Cliente(id, cpf, nome, endereco, bairro, cidadeObj, telefone, email);
+                Cliente.editar()
+                .then(() => {
+                    resposta.status(200).json({
+                        "status": true,
+                        "mensagem": "Cliente atualizado com sucesso."
+                    })
+                })
+                .catch((erro) => {
+                    resposta.status(500).json({
+                        "status": false,
+                        "mensagem": "Erro ao editar o cliente: " + erro.message
+                    })
+                });
+
+                
+            }else{
+                resposta.status(400).json({
+                    "status": false,
+                    "mensagem": "Todos os dados do cliente são obrigatórios. Consulte a documentação da API."
+                });
+            }
+
+        }else{
+            resposta.status(405).json({
+                "status": false,
+                "mensagem": "Metodo nao permitido. Consulte a documentação da API."
+            });
+        }
+     }
 
     // Requisição do tipo DELETE
-    async excluir(requisicao, resposta) {}
+     excluir(requisicao, resposta) {
+        if(requisicao.method === "DELETE"){
+            const id = requisicao.params.id;
+            if(id > 0){
+                const cliente = new Cliente(id);
+                Cliente.excluir()
+                .then(() => {
+                    resposta.status(200).json({
+                        "status": true,
+                        "mensagem": "Cliente excluido com sucesso."
+                    })
+                })
+                .catch((erro) => {
+                    resposta.status(500).json({
+                        "status": false,
+                        "mensagem": "Erro ao excluir o cliente: " + erro.message
+                    })
+                });
+
+            }
+            else{
+                resposta.status(400).json({
+                    "status": false,
+                    "mensagem": "Informe um id válido. Consulte a documentação da API."
+                });
+            }
+
+        }else{
+            resposta.status(405).json({
+                "status": false,
+                "mensagem": "Metodo nao permitido. Consulte a documentação da API."
+            });
+        }
+     }
 
     // Requisição do tipo GET
-    async consultar(requisicao, resposta) {
-        
+     consultar(requisicao, resposta) {
+        if(requisicao.method === "GET"){
+            // a identificação da existência de um id na URL, provocará a consulta por id (consultar por pessoa especifica).
+            // sem id a consulta será genérica.
+
+            const id = requisicao.params.id;
+            if(!isNaN(id)){ // id é um número?
+
+                termo = id;
+
+            }else{
+                let termo = ''; // string vazia.
+            }
+
+            const cliente = new Cliente();
+            cliente.consultar(termo)
+            .then((listaClientes) => {
+                resposta.status(200).json({
+                    "status": true,
+                    "mensagem": "Consulta realizada com sucesso.",
+                    "clientes": listaClientes
+                });
+            })
+            .catch((erro) => {
+                resposta.status(500).json({
+                    "status": false,
+                    "mensagem": "Erro ao consultar o cliente: " + erro.message
+                })
+            });
+            
+        }
+
     }
+    
 }
--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-import Cliente from "./Model/cliente.js";
-import Cidade from "./Model/cidade.js";
+// import Cliente from "./Model/cliente.js";
+// import Cidade from "./Model/cidade.js";
 
-const cidade = new Cidade(1, "Presidente Prudente", "SP");
-const cliente_atual = new Cliente(1, "359.769.588.42", "Sullyvan Ogawa", "rua1", "bairro1", cidade, "telefone1", "email1");
-const cliente_novo = new Cliente(2, "359.769.588.42", "Juliana Ogawa", "rua2", "bairro2", cidade, "telefone2", "email2");
+// const cidade = new Cidade(1, "Presidente Prudente", "SP");
+// const cliente_atual = new Cliente(1, "359.769.588.42", "Sullyvan Ogawa", "rua1", "bairro1", cidade, "telefone1", "email1");
+// const cliente_novo = new Cliente(2, "359.769.588.42", "Juliana Ogawa", "rua2", "bairro2", cidade, "telefone2", "email2");
 
-// cliente_novo.gravar()
-// .then(() => {
-//     console.log("Cliente Gravado com sucesso!")
+// // cliente_novo.gravar()
+// // .then(() => {
+// //     console.log("Cliente Gravado com sucesso!")
 
-// })
-// .catch(err => console.log(err));
+// // })
+// // .catch(err => console.log(err));
 
-// cliente_atual.gravar()
-// .then(() => {
-//     console.log("Cliente Gravado com sucesso!")
+// // cliente_atual.gravar()
+// // .then(() => {
+// //     console.log("Cliente Gravado com sucesso!")
 
-// })
-// .catch(err => console.log(err));
+// // })
+// // .catch(err => console.log(err));
 
 
-cliente_atual.consultar("Juliana Ogawa")
-.then((lista) => {
-    for (const cliente of lista) {
-        console.log(cliente.toString());
-    }
-})
-.catch((erro) => console.log(erro.menssage));
+// // cliente_atual.consultar("Juliana Ogawa")
+// // .then((lista) => {
+// //     for (const cliente of lista) {
+// //         console.log(cliente.toString());
+// //     }
+// // })
+// // .catch((erro) => console.log(erro.menssage));
 
+import express from "express";
+
+const localhost = '0.0.0.0';
+const port = 4000;
+const app = express();
+
+app.listen(port, localhost, () => console.log(`API Executando na porta ${port}`));
